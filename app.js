7613d431d91c4ff85e5005448ac29f55f9ef5db3
Sanjay2929 <sanjayjyani220@gmail.com>
2025-05-08 12:50:28 +0530

    code merge

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
+// gsap.to(".triggerScroll-boxes", {
+//   scrollTrigger: ".scrollTrigger", // start the animation when ".box" enters the viewport (once)
+//   x: 500,
+// });
+
 gsap.to(".triggerScroll-boxes", {
-  scrollTrigger: ".scrollTrigger", // start the animation when ".box" enters the viewport (once)
-  x: 500,
+  y: 100,
+  stagger: 0.1, // 0.1 seconds between when each ".box" element starts animating
 });
